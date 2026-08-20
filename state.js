e7e4d8295b1fb56f630ad305c8ab7ea54417470e
shuta1208omurozz-sketch <shuta1208omurozz@gmail.com>
2026-08-21 00:54:55 +0900

    Add files via upload

--- state.js
+++ state.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const APP_VERSION = 'FIX71';
+const APP_VERSION = 'FIX72';
 const IS_IOS_LIKE = /iPad|iPhone|iPod/.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
 
 /* ════ キー定数 ════ */
